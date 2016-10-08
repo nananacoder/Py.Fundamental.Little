e4908fd37fc41950f88095eaee627a2ea9aea320
ankiyang <398753283@qq.com>
2016-10-08 17:33:09 +0800

    add two_one_win

---
+++ Fundamentals/two_fighters_one_winner.py
@@ -0,0 +1,48 @@
+'''
+Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious.
+Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance.
+See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0.
+You can mutate the Fighter objects.
+
+declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+  // Python
+  declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+  Lew attacks Harry; Harry now has 3 health.
+  Harry attacks Lew; Lew now has 6 health.
+  Lew attacks Harry; Harry now has 1 health.
+  Harry attacks Lew; Lew now has 2 health.
+  Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+'''
+
+
+class Fighter(object):
+    def __init__(self, name, health, damage_per_attack):
+        self.name = name
+        self.health = health
+        self.damage_per_attack = damage_per_attack
+
+
+def declare_winner(fighter1, fighter2, first_attacker):
+    def fight(fighter1, fighter2):
+        while (fighter1.health > 0 or fighter2.health > 0):
+            fighter2.health -= fighter1.damage_per_attack
+            if fighter2.health <= 0:
+                return fighter1.name
+            fighter1.health -= fighter2.damage_per_attack
+            if fighter1.health <=0:
+                return fighter2.name
+
+    if first_attacker == fighter1.name:
+        return fight(fighter1, fighter2)
+    else:
+        return fight(fighter2, fighter1)
+
+print declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew")
